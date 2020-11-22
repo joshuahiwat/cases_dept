99a0915e1151d9388848ce033c132cd2497841d7
Joshua van Londen Hiwat <mail@joshualonden.com>
2020-11-22 14:07:27 +0100

    Refactor interface, didn't saw the error. Stupid refactor action (previous) haha

--- lib/helpers/outsideModuleTemplate.php
+++ lib/helpers/outsideModuleTemplate.php
@@ -13,9 +13,7 @@ interface outsideModuleTemplate
      * Create curl request and get data from IMDB
      *
      * @param string $request
-     * @param string $host
-     * @param string $key
      * @return bool|string
      */
-    public function getRequest(string $request, string $host, string $key);
+    public function getRequest(string $request);
 }
