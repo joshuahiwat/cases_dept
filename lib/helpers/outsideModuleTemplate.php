b03bdbbebf2ac291e427f9215ab3b76605c3036a
Joshua van Londen Hiwat <mail@joshualonden.com>
2020-11-22 14:05:43 +0100

    Refactor outside interface, add new paramaters

--- lib/helpers/outsideModuleTemplate.php
+++ lib/helpers/outsideModuleTemplate.php
@@ -13,7 +13,9 @@ interface outsideModuleTemplate
      * Create curl request and get data from IMDB
      *
      * @param string $request
+     * @param string $host
+     * @param string $key
      * @return bool|string
      */
-    public function getRequest(string $request);
+    public function getRequest(string $request, string $host, string $key);
 }
